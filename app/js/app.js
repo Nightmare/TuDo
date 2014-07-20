71fb4143c1371d7a6f3bd1b6cf914ccf495aa2c7
Nightmare <la_fabio@hotmail.com>
2014-07-20 17:38:39 +0200

    first commit - fixed angularjs seed errors

---
+++ app/js/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+
+// Declare app level module which depends on filters, and services
+var myApp = angular.module('myApp', [
+  'ngRoute',
+  'myAppFilters',
+  'myAppServices',
+  'myAppDirectives',
+  'myAppControllers'
+]);
+
+myApp.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
+  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
+  $routeProvider.otherwise({redirectTo: '/view1'});
+}]);
---
+++ app/js/controllers.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/* Controllers */
+
+var myAppControllers = angular.module('myAppControllers', []);
+
+myAppControllers.controller('MyCtrl1', ['$scope', function($scope) {
+
+  }])
+myAppControllers.controller('MyCtrl2', ['$scope', function($scope) {
+
+  }]);
